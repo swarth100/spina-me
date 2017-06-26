4afeca5d393b5d368f50e3f022a12d061ed9f9b6
Alberto Spina <as12015@ic.ac.uk>
2017-06-26 10:54:52 +0100

    ecosystem.config.js: Restore routing for spina.me

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     dev: {
       user: 'ubuntu',
-      host: 'ec2-34-229-225-181.compute-1.amazonaws.com',
+      host: 'spina.me',
       key: '~/Documents/AWS/spina.pem',
       ref: 'origin/dev',
       repo: 'git@gitlab.doc.ic.ac.uk:as12015/spina-me.git',
