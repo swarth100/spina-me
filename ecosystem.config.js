a6f12b586b8a2caf0dc9612b902ac3569329863f
Alberto Spina <as12015@ic.ac.uk>
2017-06-26 09:27:25 +0100

    ecosystem.config.js: Add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'spina-me',
+    script: './server.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'spina.me',
+      key: '~/Documents/AWS/spina.pem',
+      ref: 'origin/master',
+      repo: 'git@gitlab.doc.ic.ac.uk:as12015/spina-me.git',
+      path: '/home/ubuntu/spina-me',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
