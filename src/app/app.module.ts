b5c48a83e8be2cdb422d3868cefd401740c081da
Alberto Spina <as12015@ic.ac.uk>
2017-07-22 16:11:53 +0100

    src/app/app.module.ts: Add new component dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {HomeComponent} from '../home/home.component';
+import {AboutComponent} from '../about/about.component';
+import {ProjectsComponent} from '../projects/projects.component';
+import {ContactComponent} from '../contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
