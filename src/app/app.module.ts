b9e76509728b12b0b4dc30eb5e664f0c20e97582
Alberto Spina <as12015@ic.ac.uk>
2017-07-27 11:20:08 +0200

    src/app/app.module.ts: Add app.service injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
@@ -7,10 +8,10 @@ import {AboutComponent} from './about/about.component';
 import {ProjectsComponent} from './projects/projects.component';
 import {ContactComponent} from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { ErrorComponent } from './error/error.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import {AppService} from 'app/app.service';
 
 @NgModule({
   declarations: [
@@ -20,14 +21,14 @@ import { AppRoutingModule } from './app-routing.module';
     ProjectsComponent,
     ContactComponent,
     LoginComponent,
-    RegisterComponent,
     ErrorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpModule,
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
