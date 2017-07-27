92f72fb4c9c6ddbb9831c1bcf743676e72f0ecd4
Alberto Spina <as12015@ic.ac.uk>
2017-07-27 12:27:41 +0200

    src/app/app.module.ts: Add FormsModule for ng-models in forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import {AppService} from 'app/app.service';
 
 @NgModule({
@@ -27,6 +28,7 @@ import {AppService} from 'app/app.service';
     BrowserModule,
     AppRoutingModule,
     HttpModule,
+    FormsModule,
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
