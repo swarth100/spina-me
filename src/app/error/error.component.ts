0b63ea35fa909e2661cd1c2d8695427ee69b7d9f
Alberto Spina <as12015@ic.ac.uk>
2017-07-27 11:18:41 +0200

    *.component.ts: Remove css template references

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css']
 })
 export class ErrorComponent implements OnInit {
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AppService} from 'app/app.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService : AppService) { }
 
   ngOnInit() {
   }
+
+  public submit() {
+    console.log('Got submit');
+    this.appService.postAPI();
+  }
+
+  /*
+  public submit() {
+    console.log('Got submit');
+    this.http
+      .post('/api/login', {})
+      .subscribe(data => {
+        console.log(data['results']);
+      });
+  }; */
 
 }
