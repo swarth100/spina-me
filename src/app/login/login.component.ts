8e7ea87a5163558fa06ea29820d93446c20304ad
Alberto Spina <as12015@ic.ac.uk>
2017-07-28 17:35:55 +0200

    src/app/login/login.component.ts: Refactor to reflect app.service http changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {AppService} from 'app/app.service';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'app-login',
@@ -12,14 +13,26 @@ export class LoginComponent implements OnInit {
   username: any;
   password: any;
 
-  constructor(private appService: AppService) { }
+  constructor(private appService: AppService, private http: Http) { }
 
   ngOnInit() {
   }
 
+  postLogin(body) {
+    this.http
+      .post('/api/login',
+        body)
+      .subscribe(data => {
+        console.log(data);
+        console.log(data.text());
+      }, error => {
+        /* Handle login error */
+      });
+  }
+
   public submit() {
     console.log('Got submit');
-    this.appService.postLogin({
+    this.postLogin({
       'username': this.username,
       'password': this.password,
     });
