371ef6e29bef03b96c7736b8bcc3779a4de9aea6
Alberto Spina <as12015@ic.ac.uk>
2017-07-29 19:14:38 +0200

    src/app/app-routing.module.ts: Add basic routing for dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import {RouterModule, Routes} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import {HomeComponent} from './home/home.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent},
   { path: '', component: HomeComponent },
   { path: '**', component: ErrorComponent }
 ];
