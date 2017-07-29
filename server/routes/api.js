af9641f0791c3aa1eefbcfb829d3af690c810c70
Alberto Spina <as12015@ic.ac.uk>
2017-07-29 19:14:24 +0200

    server/routes/api.js: Add server-side check to return actual updated-hash user

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,10 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
+const mongooseUser = require('./../models/mongoose/user');
+const random = require('./../models/vendor/random');
+
+
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
@@ -24,7 +28,21 @@ router.post('/login', (req, res, next) => {
         return next(err);
       }
       console.log('user found');
-      return res.status(200).send(user);
+
+      /* Instantiate the random hash */
+      mongooseUser.updateHash(user, random.makeID())
+        .then(function(msg) {
+          mongooseUser.find({'username': user.username})
+            .then(function(user) {
+              return res.status(200).send(user);
+            })
+            .catch(function(err) {
+              console.log('Database error');
+            });
+        })
+        .catch(function(err) {
+          console.log('No element in the database meets the search criteria');
+        });
     });
   })(req, res, next);
 });
