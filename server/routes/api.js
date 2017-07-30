0e630df5c060403856a86b462fe93e668194f679
Alberto Spina <as12015@ic.ac.uk>
2017-07-30 13:57:49 +0200

    server/routes/api.js: Add removal of deprecated projects

--- server/routes/api.js
+++ server/routes/api.js
@@ -50,7 +50,7 @@ router.post('/login', (req, res, next) => {
 });
 
 /* Routing to require access to projects */
-router.get('/projects/:hash', function(req, res) {
+router.get('/projects', function(req, res) {
   console.log('[index.html] : GET request to /projects/' + req.params.hash);
 
   /* TODO: Use hash to perform a lookup check on users DB */
@@ -58,7 +58,7 @@ router.get('/projects/:hash', function(req, res) {
   mongooseProject.findMultiple({})
     .then(function(prjs) {
       for (let i = 0; i < prjs.length; i++) {
-        console.log(prjs[i]);
+        // console.log(prjs[i]);
       }
       res.send(prjs);
     })
@@ -69,13 +69,29 @@ router.get('/projects/:hash', function(req, res) {
 });
 
 /* Routing to update a selected project */
-router.post('/updateProjects', function(req, res) {
+router.post('/updateProjects/:hash', function(req, res) {
   console.log('[index.html] : POST request to /updateProjects/');
 
   /* TODO: Use hash to perform a lookup check on users DB */
 
   /* Updates and if not found, inserts */
   mongooseProject.updateProject(req.body)
+    .then(function(msg) {
+      /* SUCCESS */
+    })
+    .catch(function(err) {
+      console.log('No element in the database meets the search criteria');
+    });
+});
+
+/* Routing to update a selected project */
+router.post('/removeProjects/:hash', function(req, res) {
+  console.log('[index.html] : POST request to /removeProjects/');
+
+  /* TODO: Use hash to perform a lookup check on users DB */
+
+  /* Updates and if not found, inserts */
+  mongooseProject.removeProject(req.body)
     .then(function(msg) {
       /* SUCCESS */
     })
