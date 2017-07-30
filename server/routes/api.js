676d0c8db55436eee9d1c1934f2a20612db95420
Alberto Spina <as12015@ic.ac.uk>
2017-07-30 09:17:11 +0200

    server/routes/api.js: Add projects DB getter route

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,6 +3,7 @@ const passport = require('passport');
 const router = express.Router();
 
 const mongooseUser = require('./../models/mongoose/user');
+const mongooseProject = require('./../models/mongoose/project');
 const random = require('./../models/vendor/random');
 
 
@@ -11,6 +12,7 @@ router.get('/', (req, res) => {
   res.send('api works');
 });
 
+/* Routing to post login information */
 router.post('/login', (req, res, next) => {
 
   console.log('Got an API message');
@@ -45,6 +47,25 @@ router.post('/login', (req, res, next) => {
         });
     });
   })(req, res, next);
+});
+
+/* Routing to require access to projects */
+router.get('/projects/:hash', function(req, res) {
+  console.log('[index.html] : POST request to /projects/' + req.params.hash);
+
+  /* TODO: Use hash to perform a lookup check on users DB */
+
+  mongooseProject.findMultiple({})
+    .then(function(prjs) {
+      for (let i = 0; i < prjs.length; i++) {
+        console.log(prjs[i]);
+      }
+      res.send(prjs);
+    })
+    .catch(function(err) {
+      console.log('No element in the database meets the search criteria');
+      res.send({});
+    });
 });
 
 module.exports = router;
