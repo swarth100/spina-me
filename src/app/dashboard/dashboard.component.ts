311277934e12c23298b7eb12059327ec528cd0b6
Alberto Spina <as12015@ic.ac.uk>
2017-07-30 09:18:16 +0200

    src/app/dashboard/dashboard.component.ts: Add projects field to dhashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  hash: any;
+  projects: any;
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
+    this.hash = 'foo';
+    console.log('/api/projects/' + this.hash);
+    this.retrieveProjects();
+  }
+
+  retrieveProjects() {
+    this.http
+      .get('/api/projects/' + this.hash)
+      .subscribe(data => {
+        const retdata = JSON.parse(data.text());
+        console.log(retdata);
+        this.projects = retdata;
+      }, error => {
+        /* Handle login error */
+      });
   }
 
 }
