d0f8470312fa4171a4b803f68515b576c13a352d
Alberto Spina <as12015@ic.ac.uk>
2017-08-01 16:22:39 +0200

    src/assets/js/script.js: Refactor scrollspy to be generic and scaleable with more ids

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -61,23 +61,33 @@ $(document).ready(function() {
     closeOnSelect: false // Close upon selecting a date,
   });
 
-  $("#about-btn").click(function () {
+  scroll_about = function () {
     $('html, body').animate({
       scrollTop: $("#about").offset().top
     }, 500);
-  });
+  };
 
-  $("#projects-btn").click(function () {
+  $("#about-arrow").click(scroll_about);
+  $("#about-btn").click(scroll_about);
+  $("#about-nav").click(scroll_about);
+
+  scroll_projects = function () {
     $('html, body').animate({
       scrollTop: $("#projects").offset().top
     }, 500);
-  });
+  };
 
-  $("#contact-btn").click(function () {
+  $("#projects-btn").click(scroll_projects);
+  $("#projects-nav").click(scroll_projects);
+
+  scroll_contact = function () {
     $('html, body').animate({
       scrollTop: $("#contact").offset().top
     }, 500);
-  });
+  };
+
+  $("#contact-btn").click(scroll_contact);
+  $("#contact-nav").click(scroll_contact);
 });
 
 $(document).ready(initialiseComponents);
