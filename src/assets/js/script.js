fe875be8eef1a68db9a72d2c5103c34e2a15b2ff
Alberto Spina <as12015@ic.ac.uk>
2017-08-01 15:28:53 +0200

    src/assets/js/script.js: Add scrollspy initialisation to .ready()

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -50,6 +50,8 @@ $(document).ready(function() {
   /* Initialise parallax Materialize component */
   $('.parallax').parallax();
 
+  $('.scrollspy').scrollSpy();
+
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year,
