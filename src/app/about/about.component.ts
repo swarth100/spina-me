b09abde7464fe933b4872712ea955fb62748a215
Alberto Spina <alberto.spina.1996@gmail.com>
2021-04-01 09:47:50 +0100

    about.component.html: Update the about component to display more structure text

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,7 +9,9 @@ export class AboutComponent {
   title = 'About';
 
   /* */
-  story = "Hello there! I'm Alberto Spina and I'm currently studying Computer Science at Imperial College London. Even though much of my current work is University related, I've always loved to challenge myself with a number of side projects. Back in 2014 I worked with an Arduino Uno (and Python) making '#SEM2.0', a fully automated ElectroMagnetic Chessboard. I've since tried to explore other programming languages, working with Java, C, C++, Go and Javascript. You will find most of them on my GitHub/GitLab accounts, or listed below." ;
+  story = ["Software Engineer at Two Sigma.",
+  "Before that G-Research intern in the Technology Innovation group.",
+  "I'm an Imperial College graduate and passionate computer scientist; I've always loved to challenge myself with a number of side projects. It all started in 2014 I worked with an Arduino Uno (and Python) making '#SEM2.0', a fully automated ElectroMagnetic Chessboard. I've sinceexplored other programming languages, working with Java, C, C++, Python, Javascript and Typescript. You can find most of them on my GitHub/GitLab accounts, or listed below. "];
 
   /* */
   details = [
@@ -19,7 +21,7 @@ export class AboutComponent {
     },
     {
       'title': 'Age:',
-      'content': '21 years old',
+      'content': '24 years old',
     },
     {
       'title': 'Current Employment:',
